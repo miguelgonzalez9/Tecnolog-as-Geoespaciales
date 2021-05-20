dedde7f157ae17ee087dad138eefe4d385a9b557
miguelgonzalez9 <49344195+miguelgonzalez9@users.noreply.github.com>
2021-05-19 22:32:25 -0500

    Add files via upload

--- Trans_Salud.R
+++ Trans_Salud.R
@@ -7,11 +7,12 @@ library(stringr)
 library(qdap)
 ## Modificación Datos Tranferencias a Salud.
 setwd("~/OneDrive - Universidad del rosario/Tec Geo/Proyecto/Datos/Giros Sistema General de Participaciones Salud 2019-2021")
-## listzip <- dir(pattern = "*.zip") %>% lapply(unzip)
+## Datos de transferencias SGP están por cada mes del 2019. Leer datos de cada mes. 
+## dat es una lista donde cada elemntos es una base de datos de un mes del 2019.
 listxlsx <- dir(pattern = "*.xlsx")
 dat <- lapply(listxlsx, read_excel, sheet = 1, skip = 4,
               col_type = c("text", "guess", "text", "text", "text","guess", "text", "text", "text"))
-## Convertir fecha lista 6. 
+## Convertir fecha lista 6. Fecha mal leída. 
 dat[[6]][1] <- rep("2019-04-04",length(dat[[6]][[1]]))
 
 listxls <- dir(pattern = "*.xls$") %>% lapply(read_xls, sheet = 1, range = "A5:I1138",
@@ -29,16 +30,16 @@ listdat[[11]][1] <- rep("2019-03-06",length(listdat[[11]][[1]]))
 ## Juntar bases.
 merge_dat <- do.call("rbind", listdat) %>% 
         mutate(`Fecha de pago` = as.Date(`Fecha de pago`, format = "%Y-%m-%d"))
-## Valor en caracter.
+## Valor de tranferencia leida como caracter, eliminar puntos y comas y convertir en formato numérico.
 merge_dat$Valor <- gsub(",", "", merge_dat$Valor)
 merge_dat$Valor <- gsub("\\.", "", merge_dat$Valor)
 merge_dat$Valor[str_detect(merge_dat$Valor, pattern = "![:digit:]{1,}")]
 merge_dat <- merge_dat %>% mutate(Valor = as.numeric(Valor))
-## Cambiar municipios
+## Eliminar palabra "municipio" en nombre de municipio.
 mun_transf <- merge_dat[str_detect(merge_dat$`Nombre Razon Social`, regex("municipio", ignore_case = T)),]
 mun_transf$`Nombre Razon Social` <-  gsub("MUNICIPIO |MUNICIPIO DE | DPTO| DEPARTAMENTO", "",mun_transf$`Nombre Razon Social`)
 mun_transf <- mun_transf[order(mun_transf$`Nombre Razon Social`),] %>% rename(Municipio = `Nombre Razon Social`)
-## Eliminar municipios con nombres de departamentos 
+## Eliminar nombres de departamentos en nombres de municipios.
 dpto <- c("AMAZONAS",
           "ANTIOQUIA",
           "ATLANTICO",
@@ -71,7 +72,6 @@ dpto <- c("AMAZONAS",
           "VAUPÉS",
           "VICHADA")
 dpto <- paste0(dpto, "$")
-#View(cbind(mgsub(dpto, "", mun_transf$Municipio[str_detect(mun_transf$Municipio, " ")], fixed = F),mun_transf$Municipio[str_detect(mun_transf$Municipio, " ")]))
 mun_transf$Municipio <- replace(mun_transf$Municipio, which(str_detect(mun_transf$Municipio, " ")), mgsub(dpto, "", mun_transf$Municipio[str_detect(mun_transf$Municipio, " ")], fixed = F))
 mun_transf$Municipio <- replace(mun_transf$Municipio, which(str_detect(mun_transf$Municipio, " ")), gsub(" DE$", "", mun_transf$Municipio[str_detect(mun_transf$Municipio, " ")], fixed = F))
 ## Identificar municipios con nombres repetidos. 
@@ -86,14 +86,14 @@ unique(cdup$NOMBRE_MPIO)
 mun_fin <- subset(mun_transf, !(mun_transf$Municipio %in% unique(cdup$NOMBRE_MPIO)))
 ## Resumir datos por municipio y agregar valor de transferencia. 
 mun_fin <- mun_fin %>% group_by(Municipio) %>% summarise(transf = sum(Valor))
-## Join con código DANE a través de municipios
+## Join con nombre municipio como identificador.
 mun_cod <- inner_join(mun_fin, C, by = c("Municipio" ="NOMBRE_MPIO"))
 dim(mun_fin)[1] - dim(mun_cod)[1] ## Número de observaciones perdidas en el join. 
 dim(listdat[[1]])[1]- dim(mun_cod)[1] ## Número de observaciones perdidas en todo el proceso.
 
-## Probelma: municipios no tienen identificador. problema 1 Se agrupan los que tienen el mismo nombre, 
-## Sesgo en la medición. Solución: identificación municipios repetidos, eliminación de dichas observaciones. 
-## Pérdida de obs = 276. Minimizar pérdida.
+## Probelma: municipios no tienen identificador. Si se agrupan los datos por nombre se juntan los municupios con el mismo nombre. 
+##  Solución: identificación municipios repetidos, eliminación de dichas observaciones. 
+## Pérdida de obs = 276.
 
 write.csv(mun_cod, file = "Transferencias_Salud_Municipio_2019.csv")
 
